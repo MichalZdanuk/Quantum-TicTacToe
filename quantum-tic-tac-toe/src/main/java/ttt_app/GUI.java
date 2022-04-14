51af5792567411b748b9c3b082abe5ee0f01231c
MichalZdanuk <zdanuk_m@wp.pl>
2022-04-14 11:46:42 +0200

    developed GUI(added all mechanism)

--- quantum-tic-tac-toe/src/main/java/ttt_app/GUI.java
+++ quantum-tic-tac-toe/src/main/java/ttt_app/GUI.java
@@ -1,6 +1,7 @@
 package ttt_app;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -12,31 +13,107 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class GUI implements ActionListener {
+
     static Board gameBoard = new Board();
     JFrame frame;
-    JLabel infoLabel = new JLabel("Player X move", SwingConstants.CENTER);
-    JPanel doNothing = new JPanel();
-    JTextField tf2 = new JTextField();
+    ArrayList<JButton> buttonList;
+    ArrayList<JButton> buttonListInner;
+    String number;
+    JLabel infoLabel = new JLabel("Player X Turn", SwingConstants.CENTER);
+    JLabel emptyLabel = new JLabel();// "EMPTY", SwingConstants.CENTER);
 
-    ArrayList<ButtonOnBoard> buttonOnBoardList = new ArrayList<ButtonOnBoard>();
+    JPanel panel = new JPanel(new GridLayout(3, 3));
 
-    GUI(Board passedBoard) {
-        frame = new JFrame("Quantum Tic-Tac-Toe");
+    int chosenEntangledTile;
+    String chosenMark = "";
+    String bigMark = "";
+
+    GUI(Board givenBoard) {
+        frame = new JFrame("Tic-Tac-Toe");
+        buttonList = new ArrayList<JButton>();
         for (int i = 0; i < 9; i++) {
-            buttonOnBoardList.add(new ButtonOnBoard(new JButton(), ""));
-            buttonOnBoardList.get(i).getJButton().addActionListener(this);
-            frame.add(buttonOnBoardList.get(i).getJButton());
+            number = Integer.toString(i);
+            buttonList.add(new JButton());
+            buttonList.get(i).addActionListener(this);
+            frame.add(buttonList.get(i));
             if (i == 2) {
                 frame.add(infoLabel);
             } else if (i == 5) {
-                frame.add(tf2);
+                frame.add(panel);
             } else if (i == 8) {
-                frame.add(doNothing);
+                frame.add(emptyLabel);
             }
+        }
+
+        // inner
+
+        buttonListInner = new ArrayList<JButton>();
+        for (int i = 0; i < 9; i++) {
+            number = Integer.toString(i);
+            buttonListInner.add(new JButton(number));
+            buttonListInner.get(i).setVisible(false);
+            buttonListInner.get(i).addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    for (int i = 0; i < 9; i++) {
+                        if (e.getSource() == buttonListInner.get(i)) {
+                            chosenMark = buttonListInner.get(i).getText();
+                            break;
+                        }
+                    }
+                    gameBoard.resolveEntanglement(chosenMark, gameBoard.tileList.get(chosenEntangledTile));
+
+                    // highlight colapsed tiles
+                    for (int i = 0; i < gameBoard.tileList.size(); i++) {
+                        if (gameBoard.tileList.get(i).checkIfTileColapsed()) {
+                            bigMark = gameBoard.tileList.get(i).getBigMark().markSyntax();
+                            buttonList.get(gameBoard.tileList.get(i).getNumberOfTile()).setText(bigMark);
+                            buttonList.get(gameBoard.tileList.get(i).getNumberOfTile()).setEnabled(false);
+                            buttonList.get(gameBoard.tileList.get(i).getNumberOfTile())
+                                    .setBackground(Color.DARK_GRAY);
+                            buttonList.get(gameBoard.tileList.get(i).getNumberOfTile())
+                                    .setFont(new Font("TimesRoman", Font.BOLD, 50));
+                        }
+                    }
+
+                    // unlock other tiles
+                    for (int i = 0; i < 9; i++) {
+                        if (!gameBoard.tileList.get(i).checkIfTileColapsed()) {
+                            buttonList.get(i).setEnabled(true);
+                        }
+                    }
+                    // block again mark buttons
+                    for (int i = 0; i < 9; i++) {
+                        buttonListInner.get(i).setVisible(false);
+                    }
+
+                    infoLabel.setText("Player " + gameBoard.whichPlayerTurn() + " turn");
+                    resolvingEntanglementFlag = false;
+
+                    if ((!gameBoard.checkIfWinner()) && (!gameBoard.checkIfDraw())) {
+                        return;
+                    } else {
+                        if (gameBoard.draw) {
+                            infoLabel.setText("Draw!!!Nobody wins");
+                            for (int i = 0; i < 9; i++) {
+                                buttonList.get(i).setEnabled(false);
+                            }
+                            return;
+                        }
+
+                        infoLabel.setText("<html>Congratulations to player " + gameBoard.whoIsWinner()
+                                + " who has won!!!</html>");
+                        for (int i = 0; i < 9; i++) {
+                            buttonList.get(i).setEnabled(false);
+                        }
+                    }
+                }
+
+            });
+            panel.add(buttonListInner.get(i));
         }
 
         Image icon = Toolkit.getDefaultToolkit().getImage(
@@ -44,84 +121,133 @@ public class GUI implements ActionListener {
 
         frame.setIconImage(icon);
         frame.setLayout(new GridLayout(3, 4));
-        frame.setSize(600, 600);
+        frame.setSize(700, 700);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    Character mark = 'x';
+    int numberOFMove = 0;
     String text = "";
-    int numberOfMoves = 0;
-    Character mark;
     ArrayList<Integer> chosenTiles = new ArrayList<Integer>(0);
-    int previousMove;
-    ArrayList<Integer> blockedTiles = new ArrayList<Integer>(0);
+    boolean resolvingEntanglementFlag = false;
+    String additionalString = "";
 
     public void actionPerformed(ActionEvent e) {
-        // System.out.println("inside action");
+        if ((!gameBoard.checkIfWinner()) && (!gameBoard.checkIfDraw())) {
+            if (!resolvingEntanglementFlag) {
+                text = mark + Integer.toString(gameBoard.getRoundNumber());
 
-        if (gameBoard.player == false) {
-            mark = 'x';
-            infoLabel.setText("Player X move");
-        } else if (gameBoard.player == true) {
-            mark = 'o';
-            infoLabel.setText("Player O move");
-        }
+                for (int i = 0; i < buttonList.size(); i++) {
+                    if (e.getSource() == buttonList.get(i)) {
 
-        text = mark + Integer.toString(gameBoard.getRoundNumber()) + " ";
-        for (int i = 0; i < 9; i++) {
-            if (e.getSource() == buttonOnBoardList.get(i).getJButton()) {
-                buttonOnBoardList.get(i).addText(text);
-                chosenTiles.add(i);
-                blockedTiles.add(i);
-                buttonOnBoardList.get(i).getJButton().setEnabled(false);
-                break;
-            }
-        }
-        if (numberOfMoves == 1) {
-            gameBoard.makeMove(chosenTiles, "multi");
-            if (gameBoard.checkIfIsEntanglement()) {
-                System.out.println("--entangled--");
-
-                for (int i = 0; i < gameBoard.entangledTilesList.size(); i++) {
-                    buttonOnBoardList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile()).getJButton()
-                            .setBackground(Color.CYAN);
-                    buttonOnBoardList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile()).getJButton()
-                            .setForeground(Color.BLACK);
+                        buttonList.get(i).setText(buttonList.get(i).getText() + " " + text);
+                        buttonList.get(i).setEnabled(false);
+                        chosenTiles.add(i);
+                        numberOFMove++;
+                        break;
+                    }
                 }
-                infoLabel.setText("Entanglement occured");
+
+                // if player made 2 moves change turn
+                if (numberOFMove == 2) {
+                    gameBoard.makeMove(chosenTiles, "multi");
+                    for (int i = 0; i < chosenTiles.size(); i++) {
+                        buttonList.get(chosenTiles.get(i)).setEnabled(true);
+                    }
+                    changeMark();
+                    gameBoard.changePlayer();
+                    numberOFMove = 0;
+
+                    if (gameBoard.player == false) {
+                        infoLabel.setText("Player X Turn");
+                        mark = 'x';
+                    } else if (gameBoard.player == true) {
+                        infoLabel.setText("Player O Turn");
+                        mark = 'o';
+                    }
+                    chosenTiles.clear();
+                }
+
+                // checking if entanglement occured
+                if (gameBoard.checkIfIsEntanglement()) {
+
+                    infoLabel.setText("<html>Entanglement occured: player " + mark + " move</html>");
+
+                    // block all tiles
+                    for (int i = 0; i < buttonList.size(); i++) {
+                        buttonList.get(i).setEnabled(false);
+                    }
+
+                    // color entangled tiles
+                    for (int i = 0; i < gameBoard.entangledTilesList.size(); i++) {
+                        buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile()).setBackground(Color.CYAN);
+                        buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile())
+                                .setForeground(Color.BLACK);
+                        buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile()).setEnabled(true);
+                    }
+
+                    resolvingEntanglementFlag = true;
+                }
+
+            } else {
+                for (int i = 0; i < buttonList.size(); i++) {
+                    if (e.getSource() == buttonList.get(i)) {
+                        chosenEntangledTile = i;
+                        break;
+                    }
+                }
                 for (int i = 0; i < 9; i++) {
-                    buttonOnBoardList.get(i).getJButton().setEnabled(false);
+                    buttonListInner.get(i).setVisible(false);
                 }
 
+                for (int i = 0; i < gameBoard.marksInEntanglementList.size(); i++) {
+                    for (int j = 0; j < gameBoard.tileList.get(chosenEntangledTile).marklist.size(); j++) {
+                        if (gameBoard.marksInEntanglementList.get(i)
+                                .markSyntax().equals(gameBoard.tileList.get(chosenEntangledTile).marklist.get(j)
+                                        .markSyntax())) {
+                            buttonListInner.get(i).setVisible(true);
+                            text = gameBoard.marksInEntanglementList.get(i).markSyntax();
+                            buttonListInner.get(i).setText(text);
+                            break;
+                        }
+                    }
+                }
+
+                // block other tile buttons
+                for (int i = 0; i < buttonList.size(); i++) {
+                    if (!gameBoard.tileList.get(i).checkIfTileEntangled()) {
+                        buttonList.get(i).setEnabled(false);
+                    }
+                }
+
+            }
+        } else {
+            if (gameBoard.draw) {
+                infoLabel.setText("Draw!!!Nobody wins");
+                for (int i = 0; i < 9; i++) {
+                    buttonList.get(i).setEnabled(false);
+                }
+                return;
+            }
+
+            infoLabel.setText("Congratulations to player " + gameBoard.whoIsWinner() + " who has won!!!");
+            for (int i = 0; i < 9; i++) {
+                buttonList.get(i).setEnabled(false);
             }
         }
-        numberOfMoves++;
+    }
 
-        if (numberOfMoves == 2) {
-            if (!gameBoard.checkIfIsEntanglement()) {
-                for (int i = 0; i < blockedTiles.size(); i++) {
-                    buttonOnBoardList.get(blockedTiles.get(i)).getJButton().setEnabled(true);
-                }
-            }
-            numberOfMoves = 0;
-            gameBoard.changePlayer();
-            blockedTiles.clear();
-            chosenTiles.clear();
-
-            if (!gameBoard.checkIfIsEntanglement()) {
-                if (gameBoard.player == false) {
-                    infoLabel.setText("Player X move");
-                } else if (gameBoard.player == true) {
-                    infoLabel.setText("Player O move");
-                }
-            }
-
+    private void changeMark() {
+        if (mark == 'x') {
+            mark = 'o';
+        } else if (mark == 'o') {
+            mark = 'x';
         }
-
     }
 
     public static void main(String[] args) {
         new GUI(gameBoard);
-        // System.out.println("inside main");
     }
+
 }
