5a7eefde6b014c002cc90c2e96e53b30946cbdaa
MichalZdanuk <zdanuk_m@wp.pl>
2022-04-21 11:53:40 +0200

    added sounds on click and background music

--- quantum-tic-tac-toe/src/main/java/ttt_app/GUI.java
+++ quantum-tic-tac-toe/src/main/java/ttt_app/GUI.java
@@ -7,8 +7,16 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,6 +32,9 @@ public class GUI implements ActionListener {
     String number;
     JLabel infoLabel = new JLabel("Player X Turn", SwingConstants.CENTER);
     JLabel emptyLabel = new JLabel();
+    static Clip clip;
+    static Clip clickClip;
+    static Clip entanglementClip;
 
     JPanel panel = new JPanel(new GridLayout(3, 3));
 
@@ -102,9 +113,19 @@ public class GUI implements ActionListener {
                     infoLabel.setText("Player " + gameBoard.whichPlayerTurn() + " turn");
                     resolvingEntanglementFlag = false;
 
+                    entanglementClip.start();
+                    entanglementClip.setMicrosecondPosition(0);
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(600);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
                     if ((!gameBoard.checkIfWinner()) && (!gameBoard.checkIfDraw())) {
                         return;
                     } else {
+                        clickClip.close();
+                        entanglementClip.close();
+                        clip.close();
                         if (gameBoard.draw) {
                             infoLabel.setText("Draw!!!Nobody wins");
                             for (int i = 0; i < 9; i++) {
@@ -143,109 +164,98 @@ public class GUI implements ActionListener {
     String additionalString = "";
 
     public void actionPerformed(ActionEvent e) {
-        if ((!gameBoard.checkIfWinner()) && (!gameBoard.checkIfDraw())) {
-            if (!resolvingEntanglementFlag) {
-                text = mark + Integer.toString(gameBoard.getRoundNumber());
+        clickClip.start();
+        clickClip.setMicrosecondPosition(0);
 
-                for (int i = 0; i < buttonList.size(); i++) {
-                    if (e.getSource() == buttonList.get(i)) {
+        if (!resolvingEntanglementFlag) {
+            text = mark + Integer.toString(gameBoard.getRoundNumber());
 
-                        buttonList.get(i).setText(buttonList.get(i).getText() + " " + text);
-                        buttonList.get(i).setEnabled(false);
-                        chosenTiles.add(i);
-                        numberOFMove++;
-                        break;
-                    }
+            for (int i = 0; i < buttonList.size(); i++) {
+                if (e.getSource() == buttonList.get(i)) {
+
+                    buttonList.get(i).setText(buttonList.get(i).getText() + " " + text);
+                    buttonList.get(i).setEnabled(false);
+                    chosenTiles.add(i);
+                    numberOFMove++;
+                    break;
                 }
-
-                // if player made 2 moves change turn
-                if (numberOFMove == 2) {
-                    gameBoard.makeMove(chosenTiles, "multi");
-                    for (int i = 0; i < chosenTiles.size(); i++) {
-                        buttonList.get(chosenTiles.get(i)).setEnabled(true);
-                    }
-                    changeMark();
-                    gameBoard.changePlayer();
-                    numberOFMove = 0;
-
-                    if (gameBoard.player == false) {
-                        infoLabel.setText("Player X Turn");
-                        mark = 'x';
-                    } else if (gameBoard.player == true) {
-                        infoLabel.setText("Player O Turn");
-                        mark = 'o';
-                    }
-                    chosenTiles.clear();
-                }
-
-                // checking if entanglement occured
-                if (gameBoard.checkIfIsEntanglement()) {
-
-                    infoLabel.setText("<html>Entanglement occured: player " + mark + " move</html>");
-
-                    // block all tiles
-                    for (int i = 0; i < buttonList.size(); i++) {
-                        buttonList.get(i).setEnabled(false);
-                    }
-
-                    // color entangled tiles
-                    for (int i = 0; i < gameBoard.entangledTilesList.size(); i++) {
-                        buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile())
-                                .setBackground(new Color(0, 204, 187));
-                        buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile())
-                                .setForeground(new Color(0, 0, 0));
-                        buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile()).setEnabled(true);
-                    }
-
-                    resolvingEntanglementFlag = true;
-                }
-
-            } else {
-                for (int i = 0; i < buttonList.size(); i++) {
-                    if (e.getSource() == buttonList.get(i)) {
-                        chosenEntangledTile = i;
-                        break;
-                    }
-                }
-                for (int i = 0; i < 9; i++) {
-                    buttonListInner.get(i).setVisible(false);
-                }
-
-                for (int i = 0; i < gameBoard.marksInEntanglementList.size(); i++) {
-                    for (int j = 0; j < gameBoard.tileList.get(chosenEntangledTile).marklist.size(); j++) {
-                        if (gameBoard.marksInEntanglementList.get(i)
-                                .markSyntax().equals(gameBoard.tileList.get(chosenEntangledTile).marklist.get(j)
-                                        .markSyntax())) {
-                            buttonListInner.get(i).setVisible(true);
-                            text = gameBoard.marksInEntanglementList.get(i).markSyntax();
-                            buttonListInner.get(i).setText(text);
-                            break;
-                        }
-                    }
-                }
-
-                // block other tile buttons
-                for (int i = 0; i < buttonList.size(); i++) {
-                    if (!gameBoard.tileList.get(i).checkIfTileEntangled()) {
-                        buttonList.get(i).setEnabled(false);
-                    }
-                }
-
             }
-        } else {
-            if (gameBoard.draw) {
-                infoLabel.setText("Draw!!!Nobody wins");
-                for (int i = 0; i < 9; i++) {
+
+            // if player made 2 moves change turn
+            if (numberOFMove == 2) {
+                gameBoard.makeMove(chosenTiles, "multi");
+                for (int i = 0; i < chosenTiles.size(); i++) {
+                    buttonList.get(chosenTiles.get(i)).setEnabled(true);
+                }
+                changeMark();
+                gameBoard.changePlayer();
+                numberOFMove = 0;
+
+                if (gameBoard.player == false) {
+                    infoLabel.setText("Player X Turn");
+                    mark = 'x';
+                } else if (gameBoard.player == true) {
+                    infoLabel.setText("Player O Turn");
+                    mark = 'o';
+                }
+                chosenTiles.clear();
+            }
+
+            // checking if entanglement occured
+            if (gameBoard.checkIfIsEntanglement()) {
+
+                infoLabel.setText("<html>Entanglement occured: player " + mark + " move</html>");
+
+                // block all tiles
+                for (int i = 0; i < buttonList.size(); i++) {
                     buttonList.get(i).setEnabled(false);
                 }
-                return;
+
+                // color entangled tiles
+                for (int i = 0; i < gameBoard.entangledTilesList.size(); i++) {
+                    buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile())
+                            .setBackground(new Color(0, 204, 187));
+                    buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile())
+                            .setForeground(new Color(0, 0, 0));
+                    buttonList.get(gameBoard.entangledTilesList.get(i).getNumberOfTile()).setEnabled(true);
+                }
+
+                resolvingEntanglementFlag = true;
             }
 
-            infoLabel.setText("Congratulations to player " + gameBoard.whoIsWinner() + " who has won!!!");
-            for (int i = 0; i < 9; i++) {
-                buttonList.get(i).setEnabled(false);
+        } else {
+            for (int i = 0; i < buttonList.size(); i++) {
+                if (e.getSource() == buttonList.get(i)) {
+                    chosenEntangledTile = i;
+                    break;
+                }
             }
+            for (int i = 0; i < 9; i++) {
+                buttonListInner.get(i).setVisible(false);
+            }
+
+            for (int i = 0; i < gameBoard.marksInEntanglementList.size(); i++) {
+                for (int j = 0; j < gameBoard.tileList.get(chosenEntangledTile).marklist.size(); j++) {
+                    if (gameBoard.marksInEntanglementList.get(i)
+                            .markSyntax().equals(gameBoard.tileList.get(chosenEntangledTile).marklist.get(j)
+                                    .markSyntax())) {
+                        buttonListInner.get(i).setVisible(true);
+                        text = gameBoard.marksInEntanglementList.get(i).markSyntax();
+                        buttonListInner.get(i).setText(text);
+                        break;
+                    }
+                }
+            }
+
+            // block other tile buttons
+            for (int i = 0; i < buttonList.size(); i++) {
+                if (!gameBoard.tileList.get(i).checkIfTileEntangled()) {
+                    buttonList.get(i).setEnabled(false);
+                }
+            }
+
         }
+
     }
 
     private void changeMark() {
@@ -256,7 +266,26 @@ public class GUI implements ActionListener {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File("backgroundMusic.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        // click sound
+        File fileClick = new File("clickSound.wav");
+        AudioInputStream clickStream = AudioSystem.getAudioInputStream(fileClick);
+        clickClip = AudioSystem.getClip();
+        clickClip.open(clickStream);
+
+        // entanglement sound
+        File fileEntanglement = new File("entanglementSound.wav");
+        AudioInputStream entanglementStream = AudioSystem.getAudioInputStream(fileEntanglement);
+        entanglementClip = AudioSystem.getClip();
+        entanglementClip.open(entanglementStream);
+
+        clip.start();
+        clip.loop(6);
         new GUI(gameBoard);
     }
 
---
+++ quantum-tic-tac-toe/src/main/java/ttt_app/Main.java
@@ -0,0 +1,52 @@
+package ttt_app;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+//import javax.sound.sampled.*;
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("musicFile.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        String response = "";
+
+        while (!response.equals("Q")) {
+            System.out.println("P = play, S = stop, R = reset, Q = quit");
+            System.out.print("Enter your choice: ");
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P"):
+                    clip.start();
+                    break;
+                case ("S"):
+                    clip.stop();
+                    break;
+                case ("R"):
+                    clip.setMicrosecondPosition(0);
+                    break;
+                case ("Q"):
+                    clip.close();
+                    break;
+                default:
+                    System.out.println("Not a valid response");
+
+            }
+            System.out.println("outside");
+        }
+    }
+}
